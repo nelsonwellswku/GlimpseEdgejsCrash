2e4cd3eb3a04e41405abd28e3c91bd2fe7d96690
Nelson Wells <nelsonwellswku@gmail.com>
2015-04-15 19:40:54 -0500

    Display a greeting generated in an edge.js script on index page

---
+++ GlimpseEdgeCrashProject/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+using EdgeJs;
+
+namespace GlimpseEdgeCrash.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			var func = @"
+                return function(data, callback) {
+                  return callback(null, {
+                    ""Greeting"" : ""Hello Edge.js World!""
+                  });
+                }
+";
+
+			var greetingFunc = Edge.Func(func);
+			dynamic greetingObj = greetingFunc(new object()).Result;
+
+			ViewBag.Greeting = greetingObj.Greeting;
+			return View();
+		}
+	}
+}
